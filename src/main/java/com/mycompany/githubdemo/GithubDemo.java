b4e1d382d54de3c594fdd5853729853912852aab
Duong Minh Dat <Duong Minh Dat@DESKTOP-M5L00DK>
2024-04-29 10:02:35 +0700

    New Demo Git

---
+++ src/main/java/com/mycompany/githubdemo/GithubDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubdemo;
+
+/**
+ *
+ * @author Duong Minh Dat
+ */
+public class GithubDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
