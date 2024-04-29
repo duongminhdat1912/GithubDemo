841aa6a08a0f45b43d032cc52227686b28eed1e0
Duong Minh Dat <Duong Minh Dat@DESKTOP-M5L00DK>
2024-04-29 10:16:34 +0700

    new sinh vien dao

---
+++ src/main/java/SinhVien/Dao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SinhVien;
+
+/**
+ *
+ * @author Duong Minh Dat
+ */
+public class Dao {
+    public static void main(String[] args) {
+        
+    }
+}
